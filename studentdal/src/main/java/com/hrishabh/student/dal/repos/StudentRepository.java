42b2dc2e9bf078e86c8ae5a56f2cf417310a73bb
Hrishabh Kumar <hrishabh0007@gmail.com>
2020-10-08 09:37:11 +0530

    removed

--- studentdal/src/main/java/com/hrishabh/student/dal/repos/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hrishabh.student.dal.repos;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.hrishabh.student.dal.entities.Student;
-
-public interface StudentRepository extends CrudRepository<Student, Long> {
-
-}
