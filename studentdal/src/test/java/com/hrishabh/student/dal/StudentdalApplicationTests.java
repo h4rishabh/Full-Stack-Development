190d36c90af76ba44d11e20c4056c662ab503d4b
Hrishabh Kumar <hrishabh0007@gmail.com>
2020-10-08 09:39:31 +0530

    Created Data Access Layer (DAL)

---
+++ studentdal/src/test/java/com/hrishabh/student/dal/StudentdalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrishabh.student.dal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentdalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
